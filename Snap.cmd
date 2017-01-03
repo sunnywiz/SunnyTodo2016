a70fc6b5fcc5d78285d84d14987b57c53c6506b3
sunny.gulati <sunnywiz@gmail.com>
2017-01-03 09:57:41 -0500

    and more stuff.

--- Snap.cmd
+++
@@ -1,7 +0,0 @@
-pushd c:\code\SunnyTodo2016
-
-SunnyTodo2016\bin\debug\SunnyTodo2016.exe snap InterfacesUpdate2.todo
-
-SunnyTodo2016\bin\debug\SunnyTodo2016.exe report InterfacesUpdate2.todo > stackedChart.csv
-
-popd
