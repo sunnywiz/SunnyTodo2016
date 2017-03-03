d91b9228e7ecde15a19c48bf85a2db32ae6542f4
Sunny Gulati <sunny@ignew.com>
2017-03-03 01:22:41 -0500

    Cleaning out EF model -- not used at the moment.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Web.Routing;
-using SunnyTodo2016;
-using SunnyTodo2016.Data;
 using websitelogic;
 using websitelogic.ViewModels;
 
